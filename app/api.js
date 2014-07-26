9279ddd820880df4c17789463415d8db08250357
U-AbhishekGupta\Abhishek Gupta <Abhishek Gupta@AbhishekGupta.(none)>
2014-07-26 00:09:17 -0400

    added get and post

--- app/api.js
+++ app/api.js
@@ -3,7 +3,7 @@ module.exports=function(app){
 	var express = require('express'),
 			mongoose= require('mongoose'),
 			dbconfig= require('../config/db.js');
-			//Student = require('../app/models/StudentSchema');
+			Student = require('../app/models/StudentSchema');
 
 	mongoose.connect(dbconfig().url);
 	console.log('CONNECTED TO DATABASE AT: ' + dbconfig().url);
@@ -12,6 +12,40 @@ module.exports=function(app){
 
 	router.get('/', function(request, response){
 		response.json({message: "welcome to the API"});
+	})
+
+	router.route('/students')
+	.post(function(request, response){
+		var student=new Student();
+
+		student.name=request.body.name;
+
+		student.save(function(error){
+			if(error){
+				response.json({
+					success:false,
+					message:'error occurred',
+					error:error
+				});
+			}
+
+			response.json({message:'Friend made!',success:true}):
+			})
+		
+
+		
+	})
+	.get(function(request, response){
+		Student.find(function(error,data){
+			if(error){
+				response.json({
+					success:false,
+					message:'Error occurred',
+					error:error
+				});
+			}
+			response.json(data)
+		});
 	})
 
 	app.use('/api', router)
