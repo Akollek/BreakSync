9f3bee8696091ae9dfe89e0223c3332b7c38c37c
AKollek <amiel.kollek@mail.mcgill.ca>
2014-07-26 01:57:58 -0400

    find courses in the fall

--- app/courses.js
+++ app/courses.js
@@ -4,7 +4,9 @@ var pass =''
 
 var minerva = new Minerva(user, pass); // or store 'em in environment MG_USER & MG_PASS
 
-var currentCourses =[];
+var ungradedCourses =[];
+var currentCourses
+
 
 
 
@@ -12,13 +14,31 @@ minerva.getTranscript().then(function(courses) {
 for (var i = 0; i < courses.length; i++) {
 
 	if (courses[i].Grade==false) {
-		currentCourses.push(courses[i]);
+		currentCourses.push(courses[i]);// get ungraded course
 	};
 
 };
-console.log(currentCourses);
-return currentCourses	
-});//.then(function(argument) {
-	
-//})
+	//console.log("Ungraded Courses")
+	//console.log(currentCourses)
+	//console.log("In Fall:")
+	return currentCourses;	
+}).then(function(currentCourses) {
+for (var i = 0; i < currentCourses.length; i++) {
+	console.log(''+currentCourses[i].Subj)
+	console.log(''+currentCourses[i].Crse)
+		minerva.getCourses({
+    		dep: ''+currentCourses[i].Subj,
+    		number: ''+currentCourses[i].Crse, 
+    		season: 'f', //fall hardcoded, lol
+    		year: '2014' //seach for classes in fall
+	}).then(function(courses) {
+
+		for (var i = 0; i < courses.length; i++) {
+			if(courses){ //check if it returned a course
+
+			}
+		};
+	})
+};
+})
 
