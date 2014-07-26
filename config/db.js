d5bee6d299414981c90a3d3486ab9f5fc399ed4c
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2014-07-26 00:56:32 -0400

    adds db.js modified

--- config/db.js
+++ config/db.js
@@ -1,3 +1,5 @@
-module.exports={
-	url: "mongodb://breaksync_users:boob@kahana.mongohq.com:10068/breaksync_db"
+module.exports = function(){
+	return {
+		url: "mongodb://breaksync_users:boob@kahana.mongohq.com:10068/breaksync_db"
+	}
 }
