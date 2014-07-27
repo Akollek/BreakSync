80557f4f9b33bf912919e6b9a1f56163492cdc64
AKollek <amiel.kollek@mail.mcgill.ca>
2014-07-27 18:57:40 -0400

    Now parses the HTML to find all the busy times. Really messy right now, just commiting because it works. I will clean it up

--- app/busyTimes.js
+++ app/busyTimes.js
@@ -1,4 +1,5 @@
-var request = require('request')
+var request = require('request');
+var htmlparser = require("htmlparser");
 
 //for dev
 username='amiel.kollek@mail.mcgill.ca'
@@ -26,6 +27,33 @@ var login_options = {
     jar: j
     };
 
+
+
+// Parsing stuff
+
+var handler = new htmlparser.DefaultHandler(function (error, dom) {
+    if (error)
+        console.log("Parsing Error!")
+    //else
+        //console.log("Parsing Sucessful!")
+});
+var parser = new htmlparser.Parser(handler);
+
+
+
+function getTimes (doc,i) {
+  if (doc[i]["children"].length==6) {
+    return doc[i]["children"][4]["children"][3]["children"][0]["raw"]+" -- "+doc[i]["children"][4]["children"][1]["children"][0]["raw"]
+  }
+  else{
+    first=doc[i]["children"][4]["children"][3]["children"][0]["raw"]+" -- "+doc[i]["children"][4]["children"][1]["children"][0]["raw"]
+    second=doc[i]["children"][6]["children"][3]["children"][0]["raw"]+" -- "+doc[i]["children"][6]["children"][1]["children"][0]["raw"]
+    return first+"\n"+second
+  }
+}
+
+
+
 request.post(login_options,function(error, response, body) {
     // Set up options for getting schedule
 
@@ -40,10 +68,23 @@ request.post(login_options,function(error, response, body) {
           };
           //console.log(j.getCookieString(base_url))
       request.post(schedule_options,function(error, response, body){ // post to get schedule
-        console.log(body) //parse here
+        //console.log(body) //parse here
+        parser.parseComplete(body);
+        //since only second one with children will have a schedule time, we can toggle this varaible
+        times = true
+        classes=[]
+        base_doc=handler.dom[2]["children"][3]["children"][5]["children"]
+        //console.log(base_doc[17]["children"][6]["children"][3]["children"][0]["raw"])
+        for (var i = 4; i < base_doc.length; i++) {
+          if(base_doc[i]["children"]){
+            times = !times
+            if (times && base_doc[i]["children"].length > 5) {
+              console.log(getTimes(base_doc,i))
 
-      })
-})
-
+            };
+          }
+        };
+        
+})})
 
 
