6ee2f661a4e2fa4fa2555084e6a0b70b159c5e14
AKollek <amiel.kollek@mail.mcgill.ca>
2014-07-29 11:44:58 -0400

    login and 2-step signup

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,7 +1,14 @@
-function reveal() {
-  document.getElementById('modal').style.display = "block";
+function reveal(form) {
+  form.style.display = "block";
 }
 
-function hide() {
-  document.getElementById('modal').style.display = "none";
+function hide(form) {
+  form.style.display = "none";
+  document.getElementById('breaksyncForm').style.display= "block";
+  document.getElementById('mcgillForm').style.display = "none";
+}
+
+function next (current, next) {
+	document.getElementById(current).style.display= "none";
+	document.getElementById(next).style.display = "block";
 }
